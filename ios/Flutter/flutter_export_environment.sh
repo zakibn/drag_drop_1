bec5b34c9ec628f4185029b76b81b8c41758a72f
zakibn <zakariachafai77@gmail.com>
2022-11-01 23:07:45 +0100

    first commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/zakarof/7wayji/dev/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/zakarof/7wayji/flutter_app/DRAG_DROP_TYPE/drag_drop_1"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
